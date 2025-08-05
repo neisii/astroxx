98fc4ee839cabc57a9cc0d744839bdc8fa83f8ff
neisii <neisii@outlook.com>
2025-08-05 20:53:58 +0900

    ✨ 블로그 초기 세팅 및 배포 설정 완료

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind'; // ✅ 이게 권장
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  base: "/astroxx/",
+  output: "static",
+  base: "/astroxx/", // ✅ 레포 이름과 일치
+  site: "https://neisii.github.io/astroxx",
   integrations: [mdx(),tailwind()]
 });
